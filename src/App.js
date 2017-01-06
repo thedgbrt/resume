6862bebff860a70880cb1b084f281d61a25e10ec
Dagobert Renouf <dagobert.king@gmail.com>
2017-01-06 18:39:02 +0100

    Added main page content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,68 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Front-end Engineer, Dagobert Renouf, 27</h1>
+        <h1>Dagobert Renouf, 27 - Front-end Engineer</h1>
+
+        <p>I make <strong>responsive websites and apps</strong>. I'm fluent in <strong>React + Redux</strong>.</p>
+
+        <p>Here are some things I made:</p>
+
+        <ul>
+          <li>La Chronique (details) [React native app]</li>
+          <li>Euratalent (details) [responsive web app]</li>
+          <li>Locomotion, Tandala, Kaleidoscopeye, Chloe bodart (details) [responsive</li> websites]
+          <li>Virage Énergie, Shaddok (details) [React websites]</li>
+        </ul>
+
+        <p>Building websites since I was 15, I have acquired a <strong>broad set of skills</strong> as <strong>founder</strong>, <strong>co-founder</strong> and contractor:</p>
+
+        <ul>
+          <li>In 2006, in high school, I <strong>created my first website that made money</strong> (details)</li>
+          <li>In 2008, I dropped out of college and <strong>moved to Moscow to grow an e-commerce website</strong> (details)</li>
+          <li>In 2011 and 2012, I <strong>led design at ModelSociety</strong> (details)</li>
+          <li>Since 2013, I've run my own consulting business that helped companies and design agencies with <strong>development, strategy and product design</strong> (details)</li>
+        </ul>
+
+        <p>These experiences have made me <strong>autonomous</strong>, <strong>self-driven</strong> and capable of <strong>leading ambitious product goals</strong>.</p>
+
+        <p>Involved in <strong>product, design and marketing</strong> decisions, I keep the bottom line in mind. When time to ship a feature runs out, I <strong>find creative ways to keep moving forward</strong>.</p>
+
+        <p>The practice of <strong>NVC </strong>(details), coupled with my knowledge of <strong>the Enneagram and Spiral Dynamics </strong>(details) further improve my capacity to <strong>facilitate communication and progress</strong> between all members of a team.</p>
+
+        <p>If you are creating a culture of <strong>curiosity, ambition and constant learning</strong>, then we might be a winning combo.</p>
+
+        <hr/>
+
+        <h3>Skills</h3>
+        <ul>
+          <li>React and React Native 8/10</li>
+          <li>Redux 8/10</li>
+          <li>ES6 Javascript 7/10</li>
+          <li>CSS3 9/10</li>
+          <li>HTML5 9/10</li>
+          <li>Flow 7/10</li>
+          <li>Jest 8/10</li>
+          <li>SEO 9/10</li>
+          <li>product design 7/10</li>
+          <li>ux design 7/10</li>
+          <li>marketing 7/10</li>
+        </ul>
+
+        <h3>Things I've gotten into recently</h3>
+        <ul>
+          <li>functional programming (see certification)</li>
+          <li>elm (via pragmatic course)</li>
+        </ul>
+
+        <h3>Other interest</h3>
+        <ul>
+          <li>holacracy and new forms of organizations</li>
+          <li>integral theory</li>
+          <li>meditation</li>
+          <li>travel</li>
+        </ul>
+
+        <p>Contact me: <a href="mailto:dagobert@dgbrt.fr">dagobert@dgbrt.fr</a></p>
       </div>
     );
   }
