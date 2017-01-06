b846ace17df3162951cb51ce2da0ff9cc4b9539f
Dagobert Renouf <dagobert.king@gmail.com>
2017-01-06 18:25:21 +0100

    Added routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { browserHistory, Router, Route } from 'react-router';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+),document.getElementById('root'));
